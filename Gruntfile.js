8873b823911e029144078c32fc39fe27ca97b891
Shivani Narang <SN@Shivanis-MacBook-Pro.local>
2017-07-18 18:40:50 +0530

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            my_target: {
+                files: {
+                    'production/perfmatters.min.js': 'js/perfmatters.js'
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'production/print.min.css': 'css/print.css'
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyCSS: true,
+                    minifyJS: true
+                },
+                files: {
+                    'index.html': 'oindex.html'
+                }
+            },
+            
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    
+    grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin']);
+    
+};
